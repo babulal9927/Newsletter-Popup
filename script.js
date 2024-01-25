1cab4de0ca0bdd8dd0f5d17d12f168b42d91dc1d
babulal9927 <129493914+babulal9927@users.noreply.github.com>
2024-01-25 19:37:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// making a function which will toggle the class
+function toggleNewsletters(){
+    const newsletter = document.getElementById("newsletter");
+    newsletter.classList.toggle("active");
+}
